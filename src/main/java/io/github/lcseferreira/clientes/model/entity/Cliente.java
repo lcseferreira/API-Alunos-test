1bf06a3e669d642ae134f5b2b49844dd996cbc94
Lucas Ferreira <lcseferreira@hotmail.com>
2021-11-04 06:29:03 -0300

    Tratamento de erros + API finalizada

--- src/main/java/io/github/lcseferreira/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/lcseferreira/clientes/model/entity/Cliente.java
@@ -2,8 +2,11 @@ package io.github.lcseferreira.clientes.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 // ENTIDADE DO BANCO DE DADOS
@@ -20,9 +23,12 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "Campo Nome é obrigatório")
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "Campo CPF é obrigatório")
+    @CPF(message = "CPF inválido")
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
--- src/main/java/io/github/lcseferreira/clientes/rest/ClienteController.java
+++ src/main/java/io/github/lcseferreira/clientes/rest/ClienteController.java
@@ -1,4 +1,4 @@
-package io.github.lcseferreira.clientes.controller;
+package io.github.lcseferreira.clientes.rest;
 
 import io.github.lcseferreira.clientes.model.entity.Cliente;
 import io.github.lcseferreira.clientes.model.repository.ClienteRepository;
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -16,14 +18,14 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente) {
+    public Cliente salvar(@RequestBody @Valid Cliente cliente) {
         return repository.save(cliente);
     }
 
     @GetMapping("{id}")
     public Cliente acharPorId(@PathVariable Integer id) {
         return repository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
     @DeleteMapping("{id}")
@@ -34,7 +36,7 @@ public class ClienteController {
                     repository.delete(cliente);
                     return cliente;
                 })
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
     @PutMapping("{id}")
@@ -45,6 +47,6 @@ public class ClienteController {
                     clienteAtualizado.setId(cliente.getId());
                     return repository.save(clienteAtualizado);
                 })
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 }

e5bd25ce19962c050f35d30d92e41bf64d85b893
Lucas <lcseferreira@hotmail.com>
2022-10-27 18:58:06 -0300

    Modificação application.properties

--- src/main/java/io/github/lcseferreira/clientes/AlunosApplication.java
+++ src/main/java/io/github/lcseferreira/clientes/AlunosApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlunosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AlunosApplication.class, args);
     }
